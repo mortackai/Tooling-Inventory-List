b3ae0d2c1dc4d449db32959c8f46e11540e07ddd
mortackai <mattraymon@gmail.com>
2018-07-12 15:29:00 -0400

    deleted clutter
    removed a bunch of code that will not be used and added section to display part quantities if there are matching

--- MainTest.vb
+++ MainTest.vb
@@ -1,76 +1,50 @@
 Sub CommandButton1_Click()
 
 'Dim nextOpenCell As Integer
-Dim Barcode As Integer
-Dim currentWorksheet As Worksheet
-Dim searchRange As Range
-Dim componentPNColumn As integer
-Application.FindFormat.Clear
+'Dim Barcode As Long
+'Dim currentWorksheet As Worksheet
+'Dim searchRange As Range
+'Dim componentPNColumn As Integer
+'Barcode = TextBox1.Value
+'Application.FindFormat.Clear
 
-If OptionButton2.Value = True Then
+If OptionButton1.Value = True Then
     If TextBox1.Value = "" Then
         MsgBox "Please Scan Tool"
         Exit Sub
-    'loop through all sheets in the workbook
-	Else
-		For Each currentWorkSheet In ActiveWorkbook.Worksheets
-		
-        currentWorkSheet.Cells(1,1).Select
-		
-			Dim varList As New List(Of String)
-			For i As Integer = 0 To 10
-					
-				'search for all instances of P/N
-				Set searchResult = currentWorkSheet.Cells.Find(_
-					What:=TextBox1.Value, _
-					After:=ActiveCell, _
-					LookIn:=xlValues, _
-					LookAt:=xlPart, _
-					SearchOrder:=xlByRows, _
-					SearchDirection:=xlNext, _
-					MatchCase:=False, _
-					SearchFormat:=False)
-					
-					Console.WriteLine(searchResult)
-					
-					varList.add("comPN" & i)
-				Next		
-
-			'Dim qty As Long = Cells(toolRow, "C").Value
-			'sum 
+        
+    Else
+        '
+        '
+        Dim rowNum As Integer
+            Set partRow = Cells.find(TextBox1.Value, After:=ActiveCell)
+            If Not partRow Is Nothing Then
+                partRow.Select
+                MsgBox (TextBox1.Value & " found in row: " & partRow.Row)
+                Label1.Caption = Sheets("Machine 10").Cells(partRow.Row, 3)
+                Label2.Caption = Sheets("Machine 10").Cells(partRow.Row, 3)
+                Label3.Caption = Sheets("Machine 10").Cells(partRow.Row, 3)
+                Label4.Caption = Sheets("Machine 10").Cells(partRow.Row, 3)
+                
+            Else
+                MsgBox (TextBox1.Value & " not found")
+            End If
+        '
+        '
+        
+        
     End If
-ElseIf OptionButton3.Value = True Then
+    
+ElseIf OptionButton2.Value = True Then
     If TextBox1.Value = "" Then
         MsgBox "Please Scan Tool"
         Exit Sub
     Else
     
-        Dim rs As Worksheet
-        Dim lRow As Long
-        Dim strSearch As String
-
-        '~~> Set this to the relevant worksheet
-        Set rs = ActiveSheet
-
-        '~~> Search Text
-        strSearch = TextBox1
-
-        With rs
-            '~~> Remove any filters
-            .AutoFilterMode = False
-
-            lRow = .Range("A" & .Rows.Count).End(xlUp).Row
-
-            With .Range("A1:A" & lRow)
-                .AutoFilter Field:=1, Criteria1:="=*" & strSearch & "*"
-                .Offset(1, 0).SpecialCells(xlCellTypeVisible).EntireRow.Delete
-            End With
-
-            '~~> Remove any filters
-            .AutoFilterMode = False
-        End With
+        'code here to remove
+        
     End If
-	
+    
 Else
     MsgBox "Please Select An Option"
 End If
