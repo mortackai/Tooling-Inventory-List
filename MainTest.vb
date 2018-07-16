3823ab5626ca42b9a0d5e1932f6337e1c30a9b8d
mortackai <mattraymon@gmail.com>
2018-07-16 15:23:23 -0400

    testing
    seriously need to reconsider my life, also add a ton of arrays.

--- MainTest.vb
+++ MainTest.vb
@@ -1,5 +1,7 @@
 Sub CommandButton1_Click()
 
+Application.ScreenUpdating = False
+
 If OptionButton1.Value = True Then
     If TextBox1.Value = "" Then
         MsgBox "Please Scan Tool"
@@ -17,14 +19,43 @@ If OptionButton1.Value = True Then
         
         For Each ws In ActiveWorkbook.Worksheets
             If ws.Name = "Machine 10" Or ws.Name = "Machine 20" Or ws.Name = "Machine 30" Or ws.Name = "Machine 40" Then
-                sheetCount = ActiveWorksheet.CountIf(Range("Z:Z"), TextBox1.Value)
+                ws.Select
+                sheetCount = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)
                 endIndex = endIndex + sheetCount
-                MsgBox (endIndex)
+                sheetCount = 0
             Else
-            
             End If
+            
+            
         Next
-        
+		
+		'Sheets("Machine 10").Select
+        'sheet1Count = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)
+        '
+        'For i = startIndex To sheet1Count
+        '    Set partRow = Cells.find(TextBox1.Value, After:=ActiveCell)
+        '        If Not partRow Is Nothing Then
+        '            partRow.Select
+        '            MsgBox (TextBox1.Value & " found in row: " & partRow.Row)
+        '            Label1.Caption = Sheets("Machine 10").Cells(partRow.Row, 3)
+        '            OptionButton3.Caption = Sheets("Machine 10").Cells(partRow.Row, 1)
+        '            
+        '        Else
+        '            MsgBox (TextBox1.Value & " not found")
+        '        End If
+
+        'Sheets("Machine 20").Select
+        'sheet2Count = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)
+        '
+        'Sheets("Machine 30").Select
+        'sheet3Count = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)
+        '
+        'Sheets("Machine 40").Select
+        'sheet4Count = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)         
+            
+        Next
+		
+        MsgBox (endIndex)
         For i = startIndex To endIndex
             '
             '
