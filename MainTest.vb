11f371ed7fb25d516ab68d21fe7f49256e63823b
mortackai <mattraymon@gmail.com>
2018-07-17 15:24:13 -0400

    added arrays and some comments
    still fails to write to second tab properly

--- MainTest.vb
+++ MainTest.vb
@@ -10,75 +10,86 @@ If OptionButton1.Value = True Then
     Else
         '
         '
-        Dim ws As Worksheet
-        Dim startIndex As Integer
-        Dim endIndex As Integer
-        Dim sheetCount As Integer
-        startIndex = 1
-        endIndex = 0
         
+        'variables
+        Dim sheetCount(3) As Integer
+        Dim totalSheetCount As Integer
+        Dim i As Integer
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+        Dim rowNum As Integer
+        Dim machineSheets(3) As String
+        Dim tool(7) As Long
+        Dim qty(7) As Long
+        machineSheets(0) = "Machine 10"
+        machineSheets(1) = "Machine 20"
+        machineSheets(2) = "Machine 30"
+        machineSheets(3) = "Machine 40"
+        i = 0
+        a = 0
+        b = 0
+        c = 0
+
+        'loop through each sheet and count instances of the searched bit
         For Each ws In ActiveWorkbook.Worksheets
             If ws.Name = "Machine 10" Or ws.Name = "Machine 20" Or ws.Name = "Machine 30" Or ws.Name = "Machine 40" Then
                 ws.Select
-                sheetCount = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)
-                endIndex = endIndex + sheetCount
-                sheetCount = 0
+                sheetCount(i) = Application.WorksheetFunction.CountIf(Range("Z1:Z200"), TextBox1.Value)
+                i = i + 1
             Else
             End If
-            
-            
-        Next
-		
-		'Sheets("Machine 10").Select
-        'sheet1Count = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)
-        '
-        'For i = startIndex To sheet1Count
-        '    Set partRow = Cells.find(TextBox1.Value, After:=ActiveCell)
-        '        If Not partRow Is Nothing Then
-        '            partRow.Select
-        '            MsgBox (TextBox1.Value & " found in row: " & partRow.Row)
-        '            Label1.Caption = Sheets("Machine 10").Cells(partRow.Row, 3)
-        '            OptionButton3.Caption = Sheets("Machine 10").Cells(partRow.Row, 1)
-        '            
-        '        Else
-        '            MsgBox (TextBox1.Value & " not found")
-        '        End If
-
-        'Sheets("Machine 20").Select
-        'sheet2Count = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)
-        '
-        'Sheets("Machine 30").Select
-        'sheet3Count = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)
-        '
-        'Sheets("Machine 40").Select
-        'sheet4Count = Application.WorksheetFunction.CountIf(Columns(26), TextBox1.Value)         
-            
-        Next
-		
-        MsgBox (endIndex)
-        For i = startIndex To endIndex
-            '
-            '
-            Dim rowNum As Integer
-                Set partRow = Cells.find(TextBox1.Value, After:=ActiveCell)
-                If Not partRow Is Nothing Then
-                    partRow.Select
-                    MsgBox (TextBox1.Value & " found in row: " & partRow.Row)
-                    Label1.Caption = Sheets("Machine 10").Cells(partRow.Row, 3)
-                    OptionButton3.Caption = Sheets("Machine 10").Cells(partRow.Row, 1)
-                    
-                Else
-                    MsgBox (TextBox1.Value & " not found")
-                End If
-            '
-            '
         Next
         
+        'go back to machine 10 sheet and reset i to 0, may remove later
+        Sheets("Machine 10").Select
+        i = 0
+        
+        'loop to find values to captions on page 2 of form
+        For Each ws In ActiveWorkbook.Worksheets
+            If ws.Name = "Machine 10" Or ws.Name = "Machine 20" Or ws.Name = "Machine 30" Or ws.Name = "Machine 40" Then
+                For i = 0 To sheetCount(i)
+                    Set partRow = Cells.find(TextBox1.Value, After:=ActiveCell)
+                    If Not partRow Is Nothing Then
+                        partRow.Select
+                        Cells(partRow.Row, 1).Select
+                        tool(b) = ActiveCell.Value
+                        Cells(partRow.Row, 3).Select
+                        qty(b) = ActiveCell.Value
+                        b = b + 1
+                        MsgBox (ws.Name)
+                    Else
+                        MsgBox ("Error, P/N not found")
+                        Exit Sub
+                    End If
+                    
+                Next
+            End If
+        Next
         
         '
         '
     End If
     
+    'assign discovered values to captions on second tab of user form
+    OptionButton3.Caption = tool(0)
+    OptionButton4.Caption = tool(1)
+    OptionButton5.Caption = tool(2)
+    OptionButton6.Caption = tool(3)
+    OptionButton7.Caption = tool(4)
+    OptionButton8.Caption = tool(5)
+    OptionButton9.Caption = tool(6)
+    OptionButton10.Caption = tool(7)
+    Label1.Caption = qty(0)
+    Label2.Caption = qty(1)
+    Label3.Caption = qty(2)
+    Label4.Caption = qty(3)
+    Label5.Caption = qty(4)
+    Label6.Caption = qty(5)
+    Label7.Caption = qty(6)
+    Label8.Caption = qty(7)
+    
+'if an option is selected but the text box is empty
 ElseIf OptionButton2.Value = True Then
     If TextBox1.Value = "" Then
         MsgBox "Please Scan Tool"
