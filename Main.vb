690b0e6d9dde125bb23cff9d20400a79be4a13c3
mortackai <mattraymon@gmail.com>
2018-07-19 09:40:58 -0400

    add and delete now work
    next addition is time stamps

--- Main.vb
+++ Main.vb
@@ -2,119 +2,133 @@ Sub CommandButton1_Click()
 
 Application.ScreenUpdating = False
 
+'variables
+Dim delta As Integer
+Dim sheetCount(3) As Integer
+Dim totalSheetCount As Integer
+Dim i As Integer
+Dim a As Integer
+Dim b As Integer
+Dim c As Integer
+Dim rowNum As Integer
+Dim machineSheets(3) As String
+Dim tool(7) As Long
+Dim qty(7) As Long
+machineSheets(0) = "Machine 10"
+machineSheets(1) = "Machine 20"
+machineSheets(2) = "Machine 30"
+machineSheets(3) = "Machine 40"
+i = 0
+a = 0
+b = 0
+c = 0
+
+'if option 1 is selected assign 1 to delta
 If OptionButton1.Value = True Then
     If TextBox1.Value = "" Then
         MsgBox "Please Scan Tool"
         Exit Sub
-        
     Else
-        '
-        '
-        
-        'variables
-        Dim sheetCount(3) As Integer
-        Dim totalSheetCount As Integer
-        Dim i As Integer
-        Dim a As Integer
-        Dim b As Integer
-        Dim c As Integer
-        Dim rowNum As Integer
-        Dim machineSheets(3) As String
-        Dim tool(7) As Long
-        Dim qty(7) As Long
-        machineSheets(0) = "Machine 10"
-        machineSheets(1) = "Machine 20"
-        machineSheets(2) = "Machine 30"
-        machineSheets(3) = "Machine 40"
-        i = 0
-        a = 0
-        b = 0
-        c = 0
-
-        'loop through each sheet and count instances of the searched bit
-        For Each ws In ActiveWorkbook.Worksheets
-            If ws.Name = "Machine 10" Or ws.Name = "Machine 20" Or ws.Name = "Machine 30" Or ws.Name = "Machine 40" Then
-                ws.Select
-                sheetCount(i) = Application.WorksheetFunction.CountIf(Range("Z1:Z200"), TextBox1.Value)
-                i = i + 1
-            Else
-            End If
-        Next
-        
-        'go back to machine 10 sheet and reset i to 0
-        Sheets("Machine 10").Select
-        i = 0
-        
-        'loop to find values to captions on page 2 of form
-        For Each ws In ActiveWorkbook.Worksheets
-            If ws.Name = "Machine 10" Or ws.Name = "Machine 20" Or ws.Name = "Machine 30" Or ws.Name = "Machine 40" Then
-                For d = 1 To sheetCount(i)
-                    ws.Select
-                    Set partRow = Cells.find(TextBox1.Value, After:=ActiveCell)
-                    If Not partRow Is Nothing Then
-                        partRow.Select
-                        Cells(partRow.Row, 1).Select
-                        tool(b) = ActiveCell.Value
-                        Cells(partRow.Row, 3).Select
-                        qty(b) = ActiveCell.Value
-                        b = b + 1
-                        Cells(partRow.Row, 27).Select
-                    Else
-                        MsgBox ("no part on this sheet")
-                        Exit Sub
-                    End If
-                    
-                Next
-                    i = i + 1
-            End If
-        Next
-        
-        '
-        '
+        delta = 1
     End If
-    
-    'assign discovered values to captions on second tab of user form
-    OptionButton3.Caption = tool(0)
-    OptionButton4.Caption = tool(1)
-    OptionButton5.Caption = tool(2)
-    OptionButton6.Caption = tool(3)
-    OptionButton7.Caption = tool(4)
-    OptionButton8.Caption = tool(5)
-    OptionButton9.Caption = tool(6)
-    OptionButton10.Caption = tool(7)
-    Label1.Caption = qty(0)
-    Label2.Caption = qty(1)
-    Label3.Caption = qty(2)
-    Label4.Caption = qty(3)
-    Label5.Caption = qty(4)
-    Label6.Caption = qty(5)
-    Label7.Caption = qty(6)
-    Label8.Caption = qty(7)
-    
-    'switch to next tab
-    MultiPage1.Value = 1
-    
-    
-'if an option is selected but the text box is empty
+
+'if option 2 is selected assign -1 to delta
 ElseIf OptionButton2.Value = True Then
     If TextBox1.Value = "" Then
         MsgBox "Please Scan Tool"
         Exit Sub
     Else
-    
-        'code here to remove
-        
+        delta = -1
     End If
-    
 Else
     MsgBox "Please Select An Option"
 End If
+
+'loop through each sheet and count instances of the searched bit
+For Each ws In ActiveWorkbook.Worksheets
+    If ws.Name = "Machine 10" Or ws.Name = "Machine 20" Or ws.Name = "Machine 30" Or ws.Name = "Machine 40" Then
+        ws.Select
+        sheetCount(i) = Application.WorksheetFunction.CountIf(Range("Z1:Z200"), TextBox1.Value)
+        i = i + 1
+    Else
+    End If
+Next
+
+'go back to machine 10 sheet and reset i to 0
+Sheets("Machine 10").Select
+i = 0
+
+'loop to find values to captions on page 2 of form
+For Each ws In ActiveWorkbook.Worksheets
+    If ws.Name = "Machine 10" Or ws.Name = "Machine 20" Or ws.Name = "Machine 30" Or ws.Name = "Machine 40" Then
+        For d = 1 To sheetCount(i)
+            ws.Select
+            Set partRow = Cells.find(TextBox1.Value, After:=ActiveCell)
+            If Not partRow Is Nothing Then
+                partRow.Select
+                Cells(partRow.Row, 1).Select
+                tool(b) = ActiveCell.Value
+                Cells(partRow.Row, 3).Select
+                qty(b) = ActiveCell.Value
+                b = b + 1
+                Cells(partRow.Row, 27).Select
+            Else
+                MsgBox ("no part on this sheet")
+                Exit Sub
+            End If
+            
+        Next
+            i = i + 1
+    End If
+Next
+    
+'assign discovered values to captions on second tab of user form
+OptionButton3.Caption = tool(0)
+OptionButton4.Caption = tool(1)
+OptionButton5.Caption = tool(2)
+OptionButton6.Caption = tool(3)
+OptionButton7.Caption = tool(4)
+OptionButton8.Caption = tool(5)
+OptionButton9.Caption = tool(6)
+OptionButton10.Caption = tool(7)
+Label1.Caption = qty(0)
+Label2.Caption = qty(1)
+Label3.Caption = qty(2)
+Label4.Caption = qty(3)
+Label5.Caption = qty(4)
+Label6.Caption = qty(5)
+Label7.Caption = qty(6)
+Label8.Caption = qty(7)
+
+'switch to next tab
+MultiPage1.Value = 1
+
 End Sub
 
 Private Sub CommandButton2_Click()
 
 'declare variables
 Dim tool As Integer
+
+If OptionButton1.Value = True Then
+    If TextBox1.Value = "" Then
+        MsgBox "Please Scan Tool"
+        Exit Sub
+    Else
+        delta = 1
+    End If
+
+'if option 2 is selected assign -1 to delta
+ElseIf OptionButton2.Value = True Then
+    If TextBox1.Value = "" Then
+        MsgBox "Please Scan Tool"
+        Exit Sub
+    Else
+        delta = -1
+    End If
+Else
+    MsgBox "Please Select An Option"
+End If
 
 
 If OptionButton3 = True Then
@@ -124,7 +138,7 @@ If OptionButton3 = True Then
         Sheets("machine 10").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -132,7 +146,7 @@ If OptionButton3 = True Then
         Sheets("machine 20").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -140,7 +154,7 @@ If OptionButton3 = True Then
         Sheets("machine 30").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -148,7 +162,7 @@ If OptionButton3 = True Then
         Sheets("machine 40").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -159,7 +173,7 @@ ElseIf OptionButton4.Value = True Then
         Sheets("machine 10").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -167,7 +181,7 @@ ElseIf OptionButton4.Value = True Then
         Sheets("machine 20").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -175,7 +189,7 @@ ElseIf OptionButton4.Value = True Then
         Sheets("machine 30").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -183,7 +197,7 @@ ElseIf OptionButton4.Value = True Then
         Sheets("machine 40").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -194,7 +208,7 @@ ElseIf OptionButton5.Value = True Then
         Sheets("machine 10").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -202,7 +216,7 @@ ElseIf OptionButton5.Value = True Then
         Sheets("machine 20").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -210,7 +224,7 @@ ElseIf OptionButton5.Value = True Then
         Sheets("machine 30").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -218,7 +232,7 @@ ElseIf OptionButton5.Value = True Then
         Sheets("machine 40").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -229,7 +243,7 @@ ElseIf OptionButton6.Value = True Then
         Sheets("machine 10").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -237,7 +251,7 @@ ElseIf OptionButton6.Value = True Then
         Sheets("machine 20").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -245,7 +259,7 @@ ElseIf OptionButton6.Value = True Then
         Sheets("machine 30").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -253,7 +267,7 @@ ElseIf OptionButton6.Value = True Then
         Sheets("machine 40").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -264,7 +278,7 @@ ElseIf OptionButton7.Value = True Then
         Sheets("machine 10").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -272,7 +286,7 @@ ElseIf OptionButton7.Value = True Then
         Sheets("machine 20").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -280,7 +294,7 @@ ElseIf OptionButton7.Value = True Then
         Sheets("machine 30").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -288,7 +302,7 @@ ElseIf OptionButton7.Value = True Then
         Sheets("machine 40").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -299,7 +313,7 @@ ElseIf OptionButton8.Value = True Then
         Sheets("machine 10").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -307,7 +321,7 @@ ElseIf OptionButton8.Value = True Then
         Sheets("machine 20").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -315,7 +329,7 @@ ElseIf OptionButton8.Value = True Then
         Sheets("machine 30").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -323,7 +337,7 @@ ElseIf OptionButton8.Value = True Then
         Sheets("machine 40").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -334,7 +348,7 @@ ElseIf OptionButton9.Value = True Then
         Sheets("machine 10").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -350,7 +364,7 @@ ElseIf OptionButton9.Value = True Then
         Sheets("machine 30").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -358,7 +372,7 @@ ElseIf OptionButton9.Value = True Then
         Sheets("machine 40").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -369,7 +383,7 @@ ElseIf OptionButton10.Value = True Then
         Sheets("machine 10").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
 
@@ -377,7 +391,7 @@ ElseIf OptionButton10.Value = True Then
         Sheets("machine 20").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -385,7 +399,7 @@ ElseIf OptionButton10.Value = True Then
         Sheets("machine 30").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
@@ -393,7 +407,7 @@ ElseIf OptionButton10.Value = True Then
         Sheets("machine 40").Select
         Cells(1, 1).Select
         Range("A:A").find(tool, After:=ActiveCell).Select
-        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + 1
+        ActiveCell.Offset(0, 2).Value = ActiveCell.Offset(0, 2).Value + delta
         Exit Sub
     End If
     
